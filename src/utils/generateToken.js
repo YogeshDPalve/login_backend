9ef1a80d7e006a639402acc020fa5f840eef7579
Yogesh Palve <dnyyogesh@gmail.com>
2025-05-12 13:01:42 +0530

    cokies setting updated for cors

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,26 +1,33 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const generateToken = (user, res, msg) => {
-    const token = jsonwebtoken_1.default.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res
-        .status(200)
-        .cookie("token", token, {
-        httpOnly: true,
-        sameSite: "strict",
-        maxAge: 24 * 60 * 60 * 1000, //! for 1 day
+  const token = jsonwebtoken_1.default.sign(
+    { userId: user._id },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" }
+  );
+  res
+    .status(200)
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000,
     })
-        .send({
-        success: true,
-        message: msg,
-        token,
-        userInfo: {
-            name: `${user.firstName} ${user.lastName}`,
-            email: user.email,
-        },
+    .send({
+      success: true,
+      message: msg,
+      token,
+      userInfo: {
+        name: `${user.firstName} ${user.lastName}`,
+        email: user.email,
+      },
     });
 };
 exports.default = generateToken;
